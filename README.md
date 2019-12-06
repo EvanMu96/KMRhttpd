628be289b4df3ead8057ad883ea18da67ccdeb9c
EvanMu96 <senmu2-c@my.cityu.edu.hk>
2019-12-07 00:44:55 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # KMRhttpd
 kqueue based C++ non-blocking http server on Unix.
+
+## usage
+`
+$ make
+./KMRhttpd # 8000 as default port.
+`
--- main.cpp
+++ main.cpp
@@ -1,22 +1,194 @@
 #include <iostream>
 #include <sys/socket.h>
 #include <sys/event.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <sys/stat.h>
 #include <arpa/inet.h>
+#include <cctype>
 #include <fcntl.h>
-#include <errno.h>
+#include <unistd.h>
 #include <string>
 #include <stdlib.h>
+#include <functional>
 
-const int kReadEvent = 1;
-const int kWriteEvent = 2;
+#define SERVER_STRING "SERVER: httpd/0.1.0\r\n"
 
+void bad_request(int);
+void cat(int, FILE *);
+void cannot_execute(int);
+int get_line(int, char *, int);
+void headers(int, const char *);
+void not_found(int);
+void serve_file(int, const char *);
+void unimplemented(int);
+void setNonBlock(int fd);
+void accept_request(int client);
+void handleAccept(int efd, int fd);
+static const int kReadEvent = 1;
+static const int kWriteEvent = 2;
+
+static char buf[1024];
+static char method[255];
+static char url[255];
+static char path[512];
+
+
+/**********************************************************************/
+/* A request has caused a call to accept() on the server port to
+ * return.  Process the request appropriately.
+ * Parameters: the socket connected to the client */
+/**********************************************************************/
+void accept_request(int client)
+{
+    int numchars;
+    size_t i, j;
+    struct stat st;
+    int cgi = 0;      /* becomes true if server decides this is a CGI
+                       * program */
+    char *query_string = NULL;
+
+    numchars = get_line(client, buf, sizeof(buf));
+    i = 0; j = 0;
+    while (!isspace(buf[j]) && (i < sizeof(method) - 1))
+    {
+        method[i] = buf[j];
+        i++; j++;
+    }
+    method[i] = '\0';
+
+    if (strcasecmp(method, "GET") && strcasecmp(method, "POST"))
+    {
+        unimplemented(client);
+        return;
+    }
+
+    if (strcasecmp(method, "POST") == 0)
+        cgi = 1;
+
+    i = 0;
+    while (isspace(buf[j]) && (j < sizeof(buf)))
+        j++;
+    while (!isspace(buf[j]) && (i < sizeof(url) - 1) && (j < sizeof(buf)))
+    {
+        url[i] = buf[j];
+        i++; j++;
+    }
+    url[i] = '\0';
+
+    if (strcasecmp(method, "GET") == 0)
+    {
+        query_string = url;
+        while ((*query_string != '?') && (*query_string != '\0'))
+            query_string++;
+        if (*query_string == '?')
+        {
+            cgi = 1;
+            *query_string = '\0';
+            query_string++;
+        }
+    }
+
+    sprintf(path, "statics%s", url);
+    printf(path);
+    printf("\n");
+    if (path[strlen(path) - 1] == '/')
+        strcat(path, "index.html");
+    if (stat(path, &st) == -1) {
+        while ((numchars > 0) && strcmp("\n", buf))  /* read & discard headers */
+            numchars = get_line(client, buf, sizeof(buf));
+        not_found(client);
+    }
+    else
+    {
+        if ((st.st_mode & S_IFMT) == S_IFDIR)
+            strcat(path, "/index.html");
+        if ((st.st_mode & S_IXUSR) ||
+            (st.st_mode & S_IXGRP) ||
+            (st.st_mode & S_IXOTH))
+            cgi = 1;
+        if (!cgi)
+            serve_file(client, path);
+        else
+            perror("cgi needed");
+            //execute_cgi(client, path, method, query_string);
+    }
+
+    close(client);
+}
+
+/**********************************************************************/
+/* Inform the client that a request it has made has a problem.
+ * Parameters: client socket */
+/**********************************************************************/
+void bad_request(int client)
+{
+    //char buf[1024];
+
+    sprintf(buf, "HTTP/1.0 400 BAD REQUEST\r\n");
+    send(client, buf, sizeof(buf), 0);
+    sprintf(buf, "Content-type: text/html\r\n");
+    send(client, buf, sizeof(buf), 0);
+    sprintf(buf, "\r\n");
+    send(client, buf, sizeof(buf), 0);
+    sprintf(buf, "<P>Your browser sent a bad request, ");
+    send(client, buf, sizeof(buf), 0);
+    sprintf(buf, "such as a POST without a Content-Length.\r\n");
+    send(client, buf, sizeof(buf), 0);
+}
+
+/**********************************************************************/
+/* Put the entire contents of a file out on a socket.  This function
+ * is named after the UNIX "cat" command, because it might have been
+ * easier just to do something like pipe, fork, and exec("cat").
+ * Parameters: the client socket descriptor
+ *             FILE pointer for the file to cat */
+/**********************************************************************/
+void cat(int client, FILE *resource)
+{
+
+    char buf[1024];
+
+    fgets(buf, sizeof(buf), resource);
+    while (!feof(resource))
+    {
+        send(client, buf, strlen(buf), 0);
+        fgets(buf, sizeof(buf), resource);
+    }
+}
+
+/**********************************************************************/
+/* Inform the client that a CGI script could not be executed.
+ * Parameter: the client socket descriptor. */
+/**********************************************************************/
+void cannot_execute(int client)
+{
+    char buf[1024];
+
+    sprintf(buf, "HTTP/1.0 500 Internal Server Error\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "Content-type: text/html\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "<P>Error prohibited CGI execution.\r\n");
+    send(client, buf, strlen(buf), 0);
+}
+
+/**********************************************************************/
+/* Print out an error message with perror() (for system errors; based
+ * on value of errno, which indicates system call errors) and exit the
+ * program indicating an error. */
+/**********************************************************************/
 inline void error_die(const char * sc)
 {
     perror(sc);
     exit(1);
 }
-
-void updateEvents(int efd, int fd, int events, bool modify)
+/**********************************************************************/
+/* Update the status of fds */
+/**********************************************************************/
+static void updateEvents(int efd, int fd, int events, bool modify)
 {
     struct kevent ev[2];
     int n = 0;
@@ -33,9 +205,279 @@ void updateEvents(int efd, int fd, int events, bool modify)
     printf("%s fd %d events read %d write %d\n",
            modify ? "mod" : "add", fd, events & kReadEvent, events & kWriteEvent);
     int r = kevent(efd, ev, n, NULL, 0, NULL);
-    error_die("kevent failed");
+    if(r < 0 ) error_die("kevent failed");
 }
 
+/**********************************************************************/
+/* Execute a CGI script.  Will need to set environment variables as
+ * appropriate.
+ * Parameters: client socket descriptor
+ *             path to the CGI script */
+/**********************************************************************/
+void execute_cgi(int client, const char *path,
+                 const char *method, const char *query_string)
+{
+    //char buf[1024];
+    int cgi_output[2];
+    int cgi_input[2];
+    pid_t pid;
+    int status;
+    int i;
+    char c;
+    int numchars = 1;
+    int content_length = -1;
+
+    buf[0] = 'A'; buf[1] = '\0';
+    if (strcasecmp(method, "GET") == 0)
+        while ((numchars > 0) && strcmp("\n", buf))  /* read & discard headers */
+            numchars = get_line(client, buf, sizeof(buf));
+    else    /* POST */
+    {
+        numchars = get_line(client, buf, sizeof(buf));
+        while ((numchars > 0) && strcmp("\n", buf))
+        {
+            buf[15] = '\0';
+            if (strcasecmp(buf, "Content-Length:") == 0)
+                content_length = atoi(&(buf[16]));
+            numchars = get_line(client, buf, sizeof(buf));
+        }
+        if (content_length == -1) {
+            bad_request(client);
+            return;
+        }
+    }
+
+    sprintf(buf, "HTTP/1.0 200 OK\r\n");
+    send(client, buf, strlen(buf), 0);
+
+    if (pipe(cgi_output) < 0) {
+        cannot_execute(client);
+        return;
+    }
+    if (pipe(cgi_input) < 0) {
+        cannot_execute(client);
+        return;
+    }
+
+    if ( (pid = fork()) < 0 ) {
+        cannot_execute(client);
+        return;
+    }
+    if (pid == 0)  /* child: CGI script */
+    {
+        char meth_env[255];
+        char query_env[255];
+        char length_env[255];
+
+        dup2(cgi_output[1], 1);
+        dup2(cgi_input[0], 0);
+        close(cgi_output[0]);
+        close(cgi_input[1]);
+        sprintf(meth_env, "REQUEST_METHOD=%s", method);
+        putenv(meth_env);
+        if (strcasecmp(method, "GET") == 0) {
+            sprintf(query_env, "QUERY_STRING=%s", query_string);
+            putenv(query_env);
+        }
+        else {   /* POST */
+            sprintf(length_env, "CONTENT_LENGTH=%d", content_length);
+            putenv(length_env);
+        }
+        execl(path, path, NULL);
+        exit(0);
+    } else {    /* parent */
+        close(cgi_output[1]);
+        close(cgi_input[0]);
+        if (strcasecmp(method, "POST") == 0)
+            for (i = 0; i < content_length; i++) {
+                recv(client, &c, 1, 0);
+                write(cgi_input[1], &c, 1);
+            }
+        while (read(cgi_output[0], &c, 1) > 0)
+            send(client, &c, 1, 0);
+
+        close(cgi_output[0]);
+        close(cgi_input[1]);
+        waitpid(pid, &status, 0);
+    }
+}
+
+
+void handleAccept(int efd, int fd) {
+    struct sockaddr_in raddr;
+    socklen_t rsz = sizeof(raddr);
+    int cfd = accept(fd,(struct sockaddr *)&raddr,&rsz);
+    if(cfd < 0) error_die("socket error");
+    sockaddr_in peer, local;
+    socklen_t alen = sizeof(peer);
+    int r = getpeername(cfd, (sockaddr*)&peer, &alen);
+    if(r < 0) error_die("get peer name failed");
+    printf("accept a connection from %s\n", inet_ntoa(raddr.sin_addr));
+    setNonBlock(cfd);
+    updateEvents(efd, cfd, kReadEvent|kWriteEvent, false);
+}
+
+void handleRead(int efd, int fd)
+{
+  accept_request(fd);
+}
+
+
+void handleWrite(int efd, int fd) {
+    //just update
+    updateEvents(efd, fd, kReadEvent, true);
+}
+
+/**********************************************************************/
+/* Get a line from a socket, whether the line ends in a newline,
+ * carriage return, or a CRLF combination.  Terminates the string read
+ * with a null character.  If no newline indicator is found before the
+ * end of the buffer, the string is terminated with a null.  If any of
+ * the above three line terminators is read, the last character of the
+ * string will be a linefeed and the string will be terminated with a
+ * null character.
+ * Parameters: the socket descriptor
+ *             the buffer to save the data in
+ *             the size of the buffer
+ * Returns: the number of bytes stored (excluding null) */
+/**********************************************************************/
+int get_line(int sock, char *buf, int size)
+{
+    int i = 0;
+    char c = '\0';
+    int n;
+
+    while ((i < size - 1) && (c != '\n'))
+    {
+        n = recv(sock, &c, 1, 0);
+        /* DEBUG printf("%02X\n", c); */
+        if (-1 == n && errno == EAGAIN){
+            printf("never be here?\n");
+            continue ;
+        }
+        else if (n > 0)
+        {
+            if (c == '\r')
+            {
+                n = recv(sock, &c, 1, MSG_PEEK);
+                /* DEBUG printf("%02X\n", c); */
+                if ((n > 0) && (c == '\n'))
+                    recv(sock, &c, 1, 0);
+                else
+                    c = '\n';
+            }
+            buf[i] = c;
+            i++;
+        }
+        else
+            c = '\n';
+    }
+    buf[i] = '\0';
+
+    return(i);
+}
+
+/**********************************************************************/
+/* Return the informational HTTP headers about a file. */
+/* Parameters: the socket to print the headers on
+ *             the name of the file */
+/**********************************************************************/
+void headers(int client, const char *filename)
+{
+    //char buf[1024];
+    (void)filename;  /* could use filename to determine file type */
+
+    strcpy(buf, "HTTP/1.0 200 OK\r\n");
+    send(client, buf, strlen(buf), 0);
+    strcpy(buf, SERVER_STRING);
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "Content-Type: text/html\r\n");
+    send(client, buf, strlen(buf), 0);
+    strcpy(buf, "\r\n");
+    send(client, buf, strlen(buf), 0);
+}
+
+/**********************************************************************/
+/* Give a client a 404 not found status message. */
+/**********************************************************************/
+void not_found(int client)
+{
+    //char buf[1024];
+
+    sprintf(buf, "HTTP/1.0 404 NOT FOUND\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, SERVER_STRING);
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "Content-Type: text/html\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "<HTML><TITLE>Not Found</TITLE>\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "<BODY><P>The server could not fulfill\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "your request because the resource specified\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "is unavailable or nonexistent.\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "</BODY></HTML>\r\n");
+    send(client, buf, strlen(buf), 0);
+}
+
+/**********************************************************************/
+/* Send a regular file to the client.  Use headers, and report
+ * errors to client if they occur.
+ * Parameters: a pointer to a file structure produced from the socket
+ *              file descriptor
+ *             the name of the file to serve */
+/**********************************************************************/
+void serve_file(int client, const char *filename)
+{
+    FILE *resource = NULL;
+    int numchars = 1;
+    //char buf[1024];
+
+    buf[0] = 'A'; buf[1] = '\0';
+    while ((numchars > 0) && strcmp("\n", buf))  /* read & discard headers */
+        numchars = get_line(client, buf, sizeof(buf));
+
+    resource = fopen(filename, "r");
+    if (resource == NULL)
+        not_found(client);
+    else
+    {
+        headers(client, filename);
+        cat(client, resource);
+    }
+    fclose(resource);
+}
+
+/**********************************************************************/
+/* Inform the client that the requested web method has not been
+ * implemented.
+ * Parameter: the client socket */
+/**********************************************************************/
+void unimplemented(int client)
+{
+    //char buf[1024];
+
+    sprintf(buf, "HTTP/1.0 501 Method Not Implemented\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, SERVER_STRING);
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "Content-Type: text/html\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "<HTML><HEAD><TITLE>Method Not Implemented\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "</TITLE></HEAD>\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "<BODY><P>HTTP request method not supported.\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "</BODY></HTML>\r\n");
+    send(client, buf, strlen(buf), 0);
+}
 
 
 void setNonBlock(int fd)
@@ -46,6 +488,38 @@ void setNonBlock(int fd)
     if(r < 0) error_die("fcntl failed");
 }
 
+static void loop_once(int efd, int lfd, int waitms)
+{
+    struct timespec timeout;
+    timeout.tv_sec = waitms / 1000;
+    timeout.tv_nsec = (waitms % 1000) * 1000 * 1000;
+    const int kMaxEvent = 20;
+    struct kevent activeEvs[kMaxEvent];
+    // n is the number of fds
+    int n = kevent(efd, NULL, 0, activeEvs, kMaxEvent, &timeout);
+    // for loop检查每一个file descriptor
+    for(int i = 0; i < n; i++)
+    {
+        int fd = (int)(intptr_t)activeEvs[i].udata;
+        int events = activeEvs[i].filter;
+        if(events == EVFILT_READ)
+        {
+            if(fd == lfd)
+            {
+                handleAccept(efd, fd);
+            } else {
+                handleRead(efd, fd);
+            }
+        } else if(events == EVFILT_WRITE)
+        {
+            handleWrite(efd, fd);
+        } else {
+            close(fd);
+            perror("unkown event");
+            continue;
+        }
+    }
+}
 
 
 int main() {
@@ -59,9 +533,15 @@ int main() {
     addr.sin_port = htons(port);
     addr.sin_addr.s_addr = INADDR_ANY;
     int r = ::bind(listenfd, (struct sockaddr *) &addr, sizeof(struct sockaddr));
+    if(r < 0) error_die("bind failed");
+    r = listen(listenfd, 20);
     printf("listen at %d \n", port);
     setNonBlock(listenfd);
     updateEvents(epollfd, listenfd, kReadEvent, false);
-
+    for(;;)
+    {
+        loop_once(epollfd, listenfd, 1000);
+    }
+    close(listenfd);
     return 0;
 }
